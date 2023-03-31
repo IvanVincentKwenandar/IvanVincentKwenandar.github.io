017c154b6f39fc46e0f11e21bcb2aefff35a5f23
IvanVincentKwenandar <c14200138@john.petra.ac.id>
2023-03-31 08:06:27 +0700

    UTS new upload

---
+++ UTS/service-workers.js
@@ -0,0 +1,52 @@
+var CACHE_NAME = 'first-app';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/blog.html',
+  '/contact.html',
+  '/portfolio-example01.html',
+  '/styles.css',
+  '/app.js',
+  '/images/*',
+  '/Tutorial/*'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
