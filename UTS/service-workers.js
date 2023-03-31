afc08c338cb8271ab15ba2af694607affde8ba5d
IvanVincentKwenandar <c14200138@john.petra.ac.id>
2023-03-31 08:53:56 +0700

    Update service-workers.js

--- UTS/service-workers.js
+++ UTS/service-workers.js
@@ -46,7 +46,7 @@ self.addEventListener('install', function(event) {
     caches.open(CACHE_NAME)
       .then(function(cache) {
         console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+        cache.addAll(urlsToCache);
       })
   );
 });
