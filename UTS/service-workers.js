dec44a469162fdedfc391b192e57d03443665e32
IvanVincentKwenandar <c14200138@john.petra.ac.id>
2023-03-31 08:36:23 +0700

    Update service-workers.js

--- UTS/service-workers.js
+++ UTS/service-workers.js
@@ -8,7 +8,16 @@ var urlsToCache = [
   '/portfolio-example01.html',
   '/styles.css',
   '/app.js',
-  '/images/*',
+  '/images/logo.png',
+  '/images/example-work01.jpg',
+  '/images/example-work07.jpg',
+  '/images/example-work02.jpg',
+  '/images/example-work03.jpg',
+  '/images/example-work04.jpg',
+  '/images/example-work05.jpg',
+  '/images/example-work06.jpg',
+  '/images/example-work08.jpg',
+  '/images/example-work09.jpg',
   '/Tutorial/*'
 ];
 
