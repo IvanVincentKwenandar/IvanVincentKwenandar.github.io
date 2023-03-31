f55bb1e6d1aaa2ccf41f18acff74bece9a55518f
IvanVincentKwenandar <c14200138@john.petra.ac.id>
2023-03-31 09:20:40 +0700

    Update app.js

--- UTS/app.js
+++ UTS/app.js
@@ -1,1 +1,13 @@
-navigator.serviceWorker.register('service-workers.js');
+// navigator.serviceWorker.register('service-workers.js');
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('service-workers.js')
+        .then(function(registration) {
+          console.log('Service worker registered successfully');
+        })
+        .catch(function(err) {
+          console.error('Failed to register service worker', err);
+        });
+    });
+  }
