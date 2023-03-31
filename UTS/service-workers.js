dc6dde0e213af08a850ff7490998b031fb0215d5
IvanVincentKwenandar <c14200138@john.petra.ac.id>
2023-03-31 08:46:33 +0700

    Update service-workers.js

--- UTS/service-workers.js
+++ UTS/service-workers.js
@@ -51,19 +51,19 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+// self.addEventListener('activate', event => {
+//   event.waitUntil(
+//     caches.keys().then(cacheNames => {
+//       return Promise.all(
+//         cacheNames.map(cacheName => {
+//           if (cacheName !== CACHE_NAME) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
